65a1fae3b1be9e0f34ead81e28a509956753d3ee
Alejandro Cerezo <alce65@hotmail.es>
2021-11-15 15:44:07 +0100

    Add Populate for books collection

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 // const MongoClient = require('mongodb')
 import { MongoClient } from "mongodb";
+import dataBooks from "./books.json";
 
 export async function mongoConnect() {
   const user = process.env.DBUSER;
@@ -15,4 +16,24 @@ export async function mongoConnect() {
   const mongoConnect = await mongoClient.connect();
   const db = mongoConnect.db();
   return { mongoClient, db };
+}
+
+export async function booksConnect() {
+  const collection = process.env.BOOKS_COLLECTION;
+  const { mongoClient, db } = await mongoConnect();
+  const bookCollection = db.collection(collection);
+  return { mongoClient, bookCollection };
+}
+
+export async function booksPopulate() {
+  const collection = process.env.BOOKS_COLLECTION;
+  const { mongoClient, db } = await mongoConnect();
+  const list = await db.listCollections({ name: collection }).toArray();
+  if (list.length) {
+    await db.dropCollection(collection);
+  }
+  const bookCollection = db.collection(collection);
+  const result = await bookCollection.insertMany(dataBooks.books);
+  mongoClient.close();
+  return result;
 }
--- db.test.js
+++ db.test.js
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-import { mongoConnect } from "./db.js";
+import { mongoConnect, booksPopulate } from "./db.js";
+
 describe("given a connection with MongoDB", () => {
   const initialDBName = process.env.DBNAME;
   test("then should exist", async () => {
@@ -9,5 +10,23 @@ describe("given a connection with MongoDB", () => {
     expect(db).toBeTruthy();
     expect(db.databaseName).toBe(initialDBName);
     mongoClient.close();
+  });
+
+  describe("when a collection is defined", () => {
+    // Arrange
+    // const projectDir = process.cwd();
+    // loadEnvConfig(projectDir);
+    // const collectionName = "testingBooks";
+
+    test("then it should be created and populated", async () => {
+      // Arrange
+      //Act
+      const result = await booksPopulate();
+      console.log(result);
+      //Assert
+      expect(result).toBeTruthy();
+      expect(result.acknowledged).toBe(true);
+      expect(result.insertedCount).toBe(9);
+    });
   });
 });
